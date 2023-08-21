7f6dd8b5ee7b29d73eb2672e27b5304de890c3b6
Quentin Uhl <quentin.uhl@wanadoo.fr>
2023-08-21 11:11:13 +0200

    Update __about__.py

--- src/nexi/__about__.py
+++ src/nexi/__about__.py
@@ -1,8 +1,4 @@
 # SPDX-FileCopyrightText: 2023-present Quentin Uhl <quentin.uhl@wanadoo.fr>
 #
-# SPDX-License-Identifier: MIT
-__version__ = "1.0.3a"
-if __name__ == '__main__':
-    from .estimate_nexi_noiseless import estimate_nexi_noiseless
-    from .estimate_nexi import estimate_nexi
-    from .powderaverage.powderaverage import powder_average, save_data_as_npz, normalize_sigma
+# SPDX-License-Identifier: Apache 2.0
+__version__ = "1.0.4"
